3c548f02166dd658ce3a29bab0e5bd09dc0dc58a
Amy Yee <amyyee1998@gmail.com>
2020-11-07 19:22:05 -0500

    Created next.js app

--- client/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./styles.css";
-
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App />, mountNode);

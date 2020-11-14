ff098064da06fc19aa576531f3fcbf629a610970
Amy Yee <amyyee1998@gmail.com>
2020-11-13 23:29:27 -0500

    Color changes separately

--- pages/calendar/index.jsx
+++ pages/calendar/index.jsx
@@ -1,17 +1,41 @@
 import React, { useState, useEffect } from "react";
 
-import moment from 'moment'
-
 export default function Calendar() {
-  
+  let month = [];
+  for (let i = 0; i < 4; i++) {
+    month.push(<Weeks />)
+  }
   return (
-    moment.weekdaysShort().map(day => {
-    return (
-      <div key={day} className="weekday">
-        {day}
-      </div>
-    );
-})
-  )
-  
+    <div>
+      {month}
+    </div>
+  );
+}
+
+
+function Weeks() {
+  let week = [];
+  for (let i = 0; i < 7; i++) {
+    week.push(<Days color="blue" />)
+  }
+  return (
+    <div>
+      {week}
+    </div>
+  );
+}
+
+function Days() {
+  const [color, setColor] = useState("red");
+
+  return (
+    <div>
+      <button
+        style={{ backgroundColor: color }}
+        onClick={() => setColor("blue")}
+      >
+        Button1
+      </button>
+    </div>
+  );
 }

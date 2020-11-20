a08b0fefdaa4e4a5757e1113fb32ce8d37451008
Amy Yee <amyyee1998@gmail.com>
2020-11-19 21:23:27 -0500

    One column buttons change individually

--- pages/calendar/build.jsx
+++ pages/calendar/build.jsx
@@ -3,12 +3,13 @@ import React, { useState, useEffect } from "react";
 export default function Weeks() {
   let week = [];
   for (let i = 0; i < 7; i++) {
-    week.push(<Days color="blue" />);
+    week.push(<Days textContent = "Monday1" />);
   }
   return <div>{week}</div>;
 }
 
 function Days() {
+  const daysOfWeek = ['Monday', 'Tuesday', 'Weds']
   const [color, setColor] = useState("red");
 
   return (
@@ -17,8 +18,9 @@ function Days() {
         style={{ backgroundColor: color }}
         onClick={() => setColor("blue")}
       >
-        Button1
+        Monday
       </button>
+    
     </div>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import Calendar from './calendar/index.jsx';
 import moment from 'moment';
 
 export default function Home() {
-  const [value, setValue] = useState(moment());
+
 
   return (
     <div className="container">
@@ -124,36 +124,6 @@ export default function Home() {
 
           max-width: 800px;
           margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
         }
 
         .logo {
